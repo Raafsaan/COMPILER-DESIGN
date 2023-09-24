621e635d28d2c07880c137295420e775c8bcc412
RAFSAN MAHMUD <131894725+Raafsaan@users.noreply.github.com>
2023-09-24 10:28:12 +0600

    1.cpp

--- TOKEN IDENTIFICATION PART 1/1.cpp
+++ TOKEN IDENTIFICATION PART 1/1.cpp
@@ -1,21 +1,39 @@
 #include<iostream>
 using namespace std;
+
+bool check(string s)
+{
+
+    for(int i=0;i<s.size();i++)
+    {
+        if(s>="0"&&s<="9")
+    {
+
+
+    }
+
+    else if(s[i]>='A'&&s[i]<='Z'&&s[i]>='a'&&s[i]<='z')
+    {
+
+
+       return false;
+    }
+    return true;
+
+
+    }}
+
 int main()
 {
-    string a;
-    cout<<"Enter a input: ";
-    cin>>a;
 
-    if(a>="1"&&a<="9")
+    string s;
+    cin>>s;
+    if(check(s)==true)
     {
 
-        cout<<"Numeric constant"<<endl;
+        cout<<"Numeric constant";
     }
-    else
-    {
-
-        cout<<"not numeric";
+    else{
+        cout<<"Not numeric constant";
     }
-
-
 }
