cccd057b687797bfbaace9d4fcf93b858698644a
RAFSAN MAHMUD <131894725+Raafsaan@users.noreply.github.com>
2023-09-24 09:56:18 +0600

    Add files via upload

---
+++ TOKEN IDENTIFICATION PART 1/1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a;
+    cout<<"Enter a input: ";
+    cin>>a;
+
+    if(a>="1"&&a<="9")
+    {
+
+        cout<<"Numeric constant"<<endl;
+    }
+    else
+    {
+
+        cout<<"not numeric";
+    }
+
+
+}
